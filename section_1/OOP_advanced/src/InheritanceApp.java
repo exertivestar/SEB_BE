92bb6369b59746ffa4a3a1e2807f1ee0619e12dc
exertivestar <exertivestar@naver.com>
2023-01-06 02:19:38 +0900

    230106 1st

--- section_1/OOP_advanced/src/InheritanceApp.java
+++ section_1/OOP_advanced/src/InheritanceApp.java
@@ -1,16 +1,26 @@
 class Cal{ // 수정할 수 없는 클래스가 되었다고 가정
     int v1, v2;
     Cal(int v1, int v2){
-        this.v1 = v1;
+        System.out.println("Cal init!!");
+        this.v1 = v1; this.v2 = v2;
     }
+    public int sum(){return this.v1+v2;}
 }
 class Cal3 extends Cal {
+    //생성자가 있는 부모클래스를 상속받았다면, 반드시 부모클래스의 생성자를 실행시켜야한다.
+    Cal3(int v1, int v2) {
+        super(v1, v2);//부모클래스의 생성자를 실행시킨다.
+        System.out.println("Cal3 init!!");
+    }
+    public int minus(){return this.v1-v2;}
 }
 
 public class InheritanceApp {
     public static void main(String[] args) {
         Cal c = new Cal(2, 1);
-        Cal3 c3 = new Cal3();
+        Cal3 c3 = new Cal3(2,1);
+        System.out.println(c3.sum()); // 3
+        System.out.println(c3.minus()); // 1
     }
 }
 
--- section_1/OOP_basic/src/Constructor.java
+++ section_1/OOP_basic/src/Constructor.java
@@ -35,18 +35,20 @@ class Car {
 }
 class ThisExample {
     public ThisExample (){
-        System.out.println("ThisExample의 기본 생성자 호출!");
+        System.out.println("This Example의 기본 생성자 호출!");
     }
 
     public ThisExample (int x) {
         this();
-        System.out.println("ThisExample의 두 번째 생성자 호출!");
+        System.out.println("This Example의 두 번째 생성자 호출!");
     }
 }
 
 public class Constructor {
 
     public static void main(String[] args) {
+        //************Canculator 예제*********************//
+        System.out.println("***********Canculator 예제***********");
 
         Calculator c1 = new Calculator(10, 20);
         c1.sum();
@@ -56,12 +58,16 @@ public class Constructor {
         c2.sum();
         c2.avg();
 
-        //******************car 예제*********************//
+
+        //**************Car 예제*********************//
+        System.out.println("\n***********Car 예제***********");
 
         Car c = new Car("Model X", "빨간색", 250);
         System.out.println("제 차는 " + c.modelName + "이고, 컬러는 " + c.color + "입니다.");
 
+
         //******************ThisExample 예제*********************//
+        System.out.println("\n************ThisExample 예제***********");
 
         ThisExample example = new ThisExample();
         ThisExample example2 = new ThisExample(5);
